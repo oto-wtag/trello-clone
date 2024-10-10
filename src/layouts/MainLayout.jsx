742a26b324374dc324e117323859407a0623f18f
Orgha Tomal Roy <orgha.tomal@welldev.io>
2024-10-10 11:10:15 +0600

    project-setup
    Created a new React project, installed dependencies and set up the project structure

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+
+import TopBar from "@/components/TopBar";
+import SideBar from "@/components/SideBar";
+
+const MainLayout = () => {
+  const [background, setBackground] = useState("#84d5f5");
+
+  return (
+    <div
+      className="relative"
+      style={{
+        background: `${background} center / cover`,
+      }}
+    >
+      <TopBar />
+      <div className="flex">
+        <div className="flex-shrink bg-black bg-opacity-40 dark:bg-opacity-70 ">
+          <SideBar />
+        </div>
+        <div className="w-full bg-black bg-opacity-0 dark:bg-opacity-20">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
